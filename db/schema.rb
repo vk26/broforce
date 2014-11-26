8992fadbc62b9d8647e6f6abe2c4e48ee61946d7
Fox <jfoxsingularity@gmail.com>
2014-11-26 18:31:47 +0300

    clear

--- db/schema.rb
+++ db/schema.rb
@@ -35,4 +35,5 @@ ActiveRecord::Schema.define(version: 20141126133027) do
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+
 end
