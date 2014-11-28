72f9e073cf468cee844711e903885990b518e2bf
Fox <jfoxsingularity@gmail.com>
2014-11-28 11:33:33 +0300

    assets

---
+++ public/assets/vider-13b5875d3c9594a35488aa84b8be49cd.js
@@ -0,0 +1,8 @@
+$(function(){
+  $('#vider-element').each(function( {
+    var $videBg = $(this).attr('data-vide-bg');
+    // TODO: var $volume = $(this).attr('data-v
+    $(this).vide(videBg);
+  }));
+}
+;
